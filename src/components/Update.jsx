3fdb646acd0eeb3811db8dcfa54ecf7dd1dd3cf6
Biplob Hasan <fbiplobhasan2024@gmail.com>
2025-08-12 07:51:34 +0600

    find and put method add

---
+++ src/components/Update.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { useLoaderData } from "react-router-dom";
+
+const Update = () => {
+  const loadedData = useLoaderData();
+  const handleUpdateUser = (event) => {
+    const form = event.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const updateUser = { name, email };
+
+    fetch(`http://localhost:5000/users/${loadedData._id}`,{
+        method: 'PUT',
+        headers: {
+            'content-type':'application/json'
+        },
+        body: JSON.stringify(updateUser)
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+  };
+  return (
+    <div>
+      <h1>User information: {loadedData.name}</h1>
+      <form onSubmit={handleUpdateUser}>
+        <input type="text" name="name" defaultValue={loadedData?.name} id="" />
+        <br />
+        <input
+          type="email"
+          name="email"
+          defaultValue={loadedData.email}
+          id=""
+        />
+        <br />
+        <input type="submit" value="Update User" />
+      </form>
+    </div>
+  );
+};
+
+export default Update;
